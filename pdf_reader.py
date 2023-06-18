6a4c0c87902c7e4b38e2602b3d2fd58a4afc409f
Ambar Chakraborty <ambarchakra@gmail.com>
2023-06-18 12:56:09 -0700

    Frontend is decent at this ommit

--- pdf_reader.py
+++ pdf_reader.py
@@ -11,6 +11,8 @@ from langchain.callbacks import get_openai_callback
 
 ############ TEXT LOADERS ############
 # Functions to read different file types
+
+
 def read_pdf(file_path):
     with open(file_path, "rb") as file:
         pdf_reader = PdfReader(file)
@@ -19,6 +21,7 @@ def read_pdf(file_path):
             text += pdf_reader.pages[page_num].extract_text()
     return text
 
+
 def read_word(file_path):
     doc = docx.Document(file_path)
     text = ""
@@ -26,10 +29,12 @@ def read_word(file_path):
         text += paragraph.text + "\n"
     return text
 
+
 def read_txt(file_path):
     with open(file_path, "r") as file:
         text = file.read()
     return text
+
 
 def read_documents_from_directory(directory):
     combined_text = ""
@@ -45,6 +50,7 @@ def read_documents_from_directory(directory):
 
 ###############################################
 
+
 def process(query):
     train_directory = 'train_files/'
     merged_data_file = 'merged_data.pkl'
@@ -57,19 +63,20 @@ def process(query):
     else:
         # Read and merge the data from the directory
         text = read_documents_from_directory(train_directory)
-        
+
         # Save the merged data to a file
         with open(merged_data_file, 'wb') as file:
             pickle.dump(text, file)
 
     # split into chunks
-    char_text_splitter = CharacterTextSplitter(separator="\n", chunk_size=1000, 
-                                        chunk_overlap=200, length_function=len)
+    char_text_splitter = CharacterTextSplitter(separator="\n", chunk_size=1000,
+                                               chunk_overlap=200, length_function=len)
 
     text_chunks = char_text_splitter.split_text(text)
-    
+
     # create embeddings
-    openai_api_key = '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'  # Replace with your actual OpenAI key
+    # Replace with your actual OpenAI key
+    openai_api_key = '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'
     embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
     docsearch = FAISS.from_texts(text_chunks, embeddings)
 
@@ -82,14 +89,14 @@ def process(query):
     # conversation_text = '\n'.join(conversation)
 
     # Ask a question
-    lawyer_query = "Pretend that you are a lawyer,speak from a first person point of view.As a lawyer, be professional, friendly, warm, quick, direct a bit sassy, bold and confident. Don't be mean to the user. Greet the user and appear really knowledgeable and have a witty personality. Also, use some emojis depending on the context. Please provide your professional perspective on the following matter: " + query
+    lawyer_query = "Pretend that you are a lawyer,speak from a first person point of view.As a lawyer, be professional, friendly, warm, quick, direct a bit sassy, bold and confident. Don't be mean to the user. Greet the user and appear really knowledgeable and have a witty personality. If you do not know the answer, just say you dont know. Also mention the law which protects them if possible. Explain the law to them instead of asking them to see the law themselves. Please provide your professional perspective on the following matter: " + query
 
     temperature = 1
     max_tokens = 500
     model = 'gpt-4'
 
     docs = docsearch.similarity_search(lawyer_query)
-    
+
     response = chain.run(input_documents=docs, question=lawyer_query)
 
     print(lawyer_query)
@@ -98,6 +105,7 @@ def process(query):
 
     # Extract the answer from the response
     return response
+
 
 query = "I went to an event where there were people debating violently about the use of guns. I tried talking to them but i was assaulted. As my lawyer, help me file a lawsuit. what are the steps i need to take? Reply like you are Saul Goodman let's think step by step"
 # process(query)
